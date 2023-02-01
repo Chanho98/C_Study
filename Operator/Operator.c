e5e247003f40004f2457c0b1eea55b97c08ecb75
CHANHOAN <ancksghho@gmail.com>
2023-02-01 23:21:31 +0900

    Add Operator at Pointer Study

---
+++ Operator/Operator.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+
+int main()
+{
+	int x[] = {1, 3, 5, 7, 9};
+	int* ptr = x;
+
+	printf("\n\n\tPOV Operator at Pointer\n");
+	printf("---------------------------------------------------------------------------------------------------------\n");
+
+	printf("Address of Integer     :%p \n", ptr);
+	printf("Value of Pointer       :%d \n", *ptr);
+
+	printf("\nOperating *++ptr\n");
+	printf("Result                 :%d\n", *++ptr);
+	printf("Address of Integer     :%p \n", ptr);
+	printf("Value of Pointer       :%p -> JUNK \n", *ptr);
+
+	printf("\nOperating ++*ptr\n");
+	printf("Result                 :%d\n", ++*ptr);
+	printf("Address of Integer     :%p\n", ptr);
+	printf("Value of Pointer       :%d\n", *ptr);
+
+	printf("\nOperating *ptr++\n");
+	printf("Result                 :%d\n", *ptr++);
+	printf("Address of Integer     :%p\n", ptr);
+	printf("Value of Pointer       :%d\n", *ptr);
+
+	printf("\nOperating (*ptr)++\n");
+	printf("Result                 :%d\n", *ptr++);
+	printf("Address of Integer     :%p\n", ptr);
+	printf("Value of Pointer       :%d\n", *ptr);
+
+	return 0;
+}
